53c17015837aa5b871096bc2636bfbf0c6d2d50e
LeeYSL <GD4@DESKTOP-0DB2D4G>
2023-05-15 17:48:57 +0900

    20230515

--- test0515/src/main/java/gdu/kr/test0515/WriteImpl.java
+++ test0515/src/main/java/gdu/kr/test0515/WriteImpl.java
@@ -1,10 +1,17 @@
 package gdu.kr.test0515;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class WriteImpl {
 	private ArticleDao dao;
+	
+	@Autowired
 	public WriteImpl(ArticleDao dao) { //생성자
 		this.dao = dao;
 	}
+	@Autowired
 	public void write() {
 		System.out.println("WriteImpo.write 메서드 호출");
 		dao.insert();
